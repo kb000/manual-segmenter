08b2089f23a8458733b0c9b6452375a24e5ca23b
Kevin Burek <git.commit@kb0.org>
2017-05-06 02:23:47 -0400

    Add rectangle drawing, escape to cancel last rectangle.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,7 @@ namespace Segmenter
     public partial class MainWindow : Window
     {
         public string sourcePath;
+        private string filename;
 
         public MainWindow()
         {
@@ -48,12 +50,100 @@ namespace Segmenter
             if (result == true)
             {
                 // Open document 
-                string filename = dlg.FileName;
+                filename = dlg.FileName;
                 textBox1.Text = filename;
                 mainImage.Source = new BitmapImage(new Uri(filename));
                 //imageScrollViewer.MaxHeight = mainImage.Source.Height;
                 //imageScrollViewer.MaxWidth = mainImage.Source.Width;
                 imageScrollViewer.UpdateLayout();
+            }
+        }
+
+        private Point? p, q;
+
+        private void mainImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            maybeWritePoint();
+
+            if (p == null)
+            {
+                p = e.GetPosition(mainImage);
+                p = new Point(Math.Round(p?.X ?? 0), Math.Round(p?.Y ?? 0));
+                textBox1.Text = $"({p?.X}, {p?.Y})";
+            }
+            else
+            {
+                q = e.GetPosition(mainImage);
+                q = new Point(Math.Round(q?.X ?? 0), Math.Round(q?.Y ?? 0));
+                textBox2.Text = $"({q?.X}, {q?.Y})";
+                updateCanvas(p, q);
+            }
+        }
+
+        private Rectangle rect;
+
+        private void cancelRect()
+        {
+            p = null;
+            textBox1.Text = "-";
+            q = null;
+            textBox2.Text = "-";
+            if (rect != null)
+            {
+                mainCanvas.Children.Remove(rect);
+                rect = null;
+            }
+        }
+
+        private void updateCanvas(Point? p1, Point? p2)
+        {
+            if (p1 != null && p2 != null)
+            {
+                var pt1 = p1.Value;
+                var pt2 = p2.Value;
+                rect = new System.Windows.Shapes.Rectangle();
+                rect.Stroke = new SolidColorBrush(Colors.RoyalBlue);
+                rect.Fill = null;
+                rect.Width = Math.Abs(pt2.X - pt1.X);
+                rect.Height = Math.Abs(pt2.Y - pt1.Y);
+                Canvas.SetLeft(rect, min(pt1.X, pt2.X));
+                Canvas.SetTop(rect, min(pt1.Y, pt2.Y));
+                mainCanvas.Children.Add(rect);
+            }
+        }
+
+        private static double min(double x1, double x2)
+        {
+            return x1 < x2 ? x1 : x2;
+        }
+
+        private void maybeWritePoint()
+        {
+            if (p != null && q != null)
+            {
+                rect.Stroke = new SolidColorBrush(Colors.Black);
+                writeQuadToCsv(p.Value, q.Value);
+                p = null;
+                q = null;
+                rect = null;
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+            {
+                cancelRect();
+            }
+        }
+
+        private void writeQuadToCsv(Point p, Point q)
+        {
+            using (var writer = File.AppendText($"{filename}.csv"))
+            {
+                writer.WriteLine($"{p.X}, {p.Y}, {q.X}, {q.Y}");
             }
         }
     }
